36dac0b403d16d877ed33988cef4fdd4342cc472
lumakos <lumakos23@gmail.com>
2025-03-10 23:53:07 +0200

    Refactor movieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -59,59 +59,125 @@ class MovieController extends Controller
         $sort = $request->query('sort', 'latest');
         $page = $request->query('page', 1);
 
-        $listMoviesKey = $userId ? "movies_{$sort}_page_{$page}_userid_{$userId}" : "movies_{$sort}_page_{$page}";
-        $totalMoviesKey = $userId ? "movies_total_count_userid_{$userId}" : "movies_total_count";
+        $listMoviesKey = $this->generateMoviesCacheKey($userId, $sort, $page);
+        $totalMoviesKey = $this->generateTotalMoviesCacheKey($userId);
+
+        if ($cachedData = $this->getMoviesFromCache($listMoviesKey, $totalMoviesKey)) {
+            return $cachedData;
+        }
 
         try {
-            if (Cache::has($listMoviesKey) && Cache::has($totalMoviesKey)) {
-                return [
-                    'movies' => Cache::get($listMoviesKey),
-                    'totalMovies' => Cache::get($totalMoviesKey),
-                    'sort' => $sort
-                ];
-            }
-
-            $sortOptions = [
-                SortOptionsHelper::LATEST => [SortOptionsHelper::CREATED_AT, 'desc'],
-                SortOptionsHelper::LIKES => [SortOptionsHelper::LIKES, 'desc'],
-                SortOptionsHelper::HATES => [SortOptionsHelper::HATES, 'desc'],
-            ];
-
-            [$column, $direction] = $sortOptions[$sort] ?? $sortOptions[SortOptionsHelper::LATEST];
-
-            $query = Movie::with('user')
-                ->withCount([
-                    'votes as likes' => fn($q) => $q->where('vote', Vote::LIKE_VOTE),
-                    'votes as hates' => fn($q) => $q->where('vote', Vote::HATE_VOTE),
-                ])->orderBy($column, $direction);
-
-            if ($userId) {
-                $query->where('user_id', $userId);
-            }
-
-            $movies = $query->paginate(Movie::PER_PAGE);
-
+            // Get Sort Options
+            $sortOptions = $this->getSortOptions($sort);
+            // Get movies
+            $movies = $this->fetchMovies($userId, $sortOptions);
 
             // Get total movies
             $totalMovies = $userId ? Movie::where('user_id', $userId)->count() : Movie::count();
-
-            // Cache sorting list of movies per page
-            Cache::put($listMoviesKey, $movies, CacheHelper::CACHE_DURATION);
-            // Cache total num of movies
-            Cache::put($totalMoviesKey, $totalMovies, CacheHelper::CACHE_DURATION);
-
-            // Store cache key of each movie
-            foreach ($movies as $movie) {
-                Redis::sadd("movie_cache_keys_{$movie->id}", $listMoviesKey);
-            }
-            //
-            Redis::sadd(CacheHelper::MOVIE_CACHE_KEY, $listMoviesKey);
+            // Cache movies
+            $this->cacheMovies($listMoviesKey, $totalMoviesKey, $movies, $totalMovies);
 
             return compact('movies', 'totalMovies', 'sort');
         } catch (\Exception $e) {
             \Log::error('Error fetching movies: ' . $e->getMessage());
             return compact([], 0, $sort);
         }
+    }
+
+    /**
+     * @param string $listMoviesKey
+     * @param string $totalMoviesKey
+     * @param $movies
+     * @param int $totalMovies
+     * @return void
+     */
+    private function cacheMovies(string $listMoviesKey, string $totalMoviesKey, $movies, int $totalMovies): void
+    {
+        // Cache sorting list of movies per page
+        Cache::put($listMoviesKey, $movies, CacheHelper::CACHE_DURATION);
+        // Cache total num of movies
+        Cache::put($totalMoviesKey, $totalMovies, CacheHelper::CACHE_DURATION);
+
+        // Store cache key of each movie
+        foreach ($movies as $movie) {
+            Redis::sadd("movie_cache_keys_{$movie->id}", $listMoviesKey);
+        }
+
+        Redis::sadd(CacheHelper::MOVIE_CACHE_KEY, $listMoviesKey);
+    }
+
+    /**
+     * @param int|null $userId
+     * @param array $sortOptions
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    private function fetchMovies(?int $userId, array $sortOptions)
+    {
+        $query = Movie::with('user')
+            ->withCount([
+                'votes as likes' => fn($q) => $q->where('vote', Vote::LIKE_VOTE),
+                'votes as hates' => fn($q) => $q->where('vote', Vote::HATE_VOTE),
+            ])
+            ->orderBy($sortOptions[0], $sortOptions[1]);
+
+        if ($userId) {
+            $query->where('user_id', $userId);
+        }
+
+        return $query->paginate(Movie::PER_PAGE);
+    }
+
+    /**
+     * @param string $sort
+     * @return array
+     */
+    private function getSortOptions(string $sort): array
+    {
+        $sortOptions = [
+            SortOptionsHelper::LATEST => [SortOptionsHelper::CREATED_AT, 'desc'],
+            SortOptionsHelper::LIKES => [SortOptionsHelper::LIKES, 'desc'],
+            SortOptionsHelper::HATES => [SortOptionsHelper::HATES, 'desc'],
+        ];
+
+        return $sortOptions[$sort] ?? $sortOptions[SortOptionsHelper::LATEST];
+    }
+
+    /**
+     * @param string $listMoviesKey
+     * @param string $totalMoviesKey
+     * @return array|null
+     */
+    private function getMoviesFromCache(string $listMoviesKey, string $totalMoviesKey): ?array
+    {
+        if (Cache::has($listMoviesKey) && Cache::has($totalMoviesKey)) {
+            return [
+                'movies' => Cache::get($listMoviesKey),
+                'totalMovies' => Cache::get($totalMoviesKey),
+                'sort' => request()->query('sort', 'latest')
+            ];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param int|null $userId
+     * @param string $sort
+     * @param int $page
+     * @return string
+     */
+    private function generateMoviesCacheKey(?int $userId, string $sort, int $page): string
+    {
+        return $userId ? "movies_{$sort}_page_{$page}_userid_{$userId}" : "movies_{$sort}_page_{$page}";
+    }
+
+    /**
+     * @param int|null $userId
+     * @return string
+     */
+    private function generateTotalMoviesCacheKey(?int $userId): string
+    {
+        return $userId ? "movies_total_count_userid_{$userId}" : "movies_total_count";
     }
 
     /**
